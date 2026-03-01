685a1e6751893ec08cff1c420b0590ed7916667c
Sourya Singh <shourya1419@gmail.com>
2026-03-02 00:38:12 +0530

    connected django to postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'complaint_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
