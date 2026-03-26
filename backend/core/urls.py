1784dad245007ac840dc943a032b322489117b20
Sourya Singh <shourya1419@gmail.com>
2026-03-26 14:17:33 +0530

    added profile api in jwt

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import health_check
+from .views import health_check, profile
+
 
 urlpatterns = [
-    path('health', health_check),
+    path('health/', health_check),
+    path('profile/', profile),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from .serializers import UserSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def health_check(request):
-    return Response({"status": "OK"})
+    return Response({
+        "status": "OK",
+        "user": request.user.username,
+        "role": request.user.role
+    })
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
